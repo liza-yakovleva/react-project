9571beeb11d60bb034a8242a3622e50e954c17fc
Elizaveta <lizayakovleva2005@gmail.com>
2021-01-13 20:21:52 +0200

    JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDom from 'react-dom';
 
-const el = React.createElement(
-  "h1",
-  null,
-  "Hello world, React.js"
-)
+// const el = React.createElement(
+//   "h1",
+//   {
+//     id: "test",
+//     name:"title"
+//  },
+//   "Hello world, React.js"
+// )
+
+// JSX
+
+const el = <h1 id = "test" name="title">Hello world,React.js!!!</h1>
 
 ReactDom.render(el, document.getElementById("root"));
