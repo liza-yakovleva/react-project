30ccb4630b44229dd6de4a093bcc5b2392d65d4e
Elizaveta <lizayakovleva2005@gmail.com>
2021-01-13 20:54:29 +0200

    create H1 and Text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDom from 'react-dom';
 
+// function Hello() {
+//   return (
+//   <div>
+//   <h1 id="test"
+//     name="title">Hello world, React.js!!!-Component!!!</h1>
+//       <p> Hello world, React.js!!!-Component!!! Hello world, React.js!!!-Component!!!</p>
+//       <p> Hello world, React.js!!!-Component!!!Hello world, React.js!!!-Component!!! </p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div >
+//     <h1 id = "test"
+//   name = "title" > Hello world, React.js!!!-Component!!</h1> <p> Hello world, React.js!!!-Component!!!Hello world, React.js!!!-Component!!! </p> <p> Hello world, React.js!!!-Component!!!Hello world, React.js!!!-Component!!! </p> </div>
+//    )
+// }
+// PascalCase
+
 // const el = React.createElement(
 //   "h1",
 //   {
@@ -12,6 +32,33 @@ import ReactDom from 'react-dom';
 
 // JSX
 
-const el = <h1 id = "test" name="title">Hello world,React.js!!!</h1>
+// const el = <h1 id = "test" name="title">Hello world,React.js!!!</h1>
 
-ReactDom.render(el, document.getElementById("root"));
+// ReactDom.render(<App />, document.getElementById("root"));
+
+
+
+
+const H1 = () => {
+  return ( <div>
+    <h1 id = "test"
+    name = "title"> Hello world, React.js!!!-Component!! </h1></div>
+  )
+}
+      
+      const Text = () => {
+        return ( <div>
+          <p> Hello world, React.js!!!-Component!!!Hello world, React.js!!!-Component!!! </p> <p> Hello world, React.js!!!-Component!!!Hello world, React.js!!!-Component!!! </p>
+          <p> Hello world, React.js!!!-Component!!!Hello world, React.js!!!-Component!!! </p> <p> Hello world, React.js!!!-Component!!!Hello world, React.js!!!-Component!!! </p>
+        
+        </div>
+        )
+      }
+const App = () => {
+  return (<div>
+   <H1/>
+    <Text/>
+   </div>
+ )
+}
+ReactDom.render( <App/> , document.getElementById("root"));
